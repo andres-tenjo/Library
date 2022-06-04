818be73e565584e493201470c33dca50d2dc643e
andres-tenjo <andrestenjo6@hotmail.com>
2022-06-04 00:28:38 -0500

    second commit

--- books/models.py
+++ books/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
+class AuthorsModel(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class CategoriesModel(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class EditorModel(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class BooksModel(models.Model):
-    title = models.CharField(max_length=100)
-    subtitle = models.CharField(max_length=100)
+    title = models.CharField('Título', max_length=100)
+    subtitle = models.CharField('Subtitulo', max_length=100)
+    authors = models.ManyToManyField(AuthorsModel)
+    categories = models.ManyToManyField(CategoriesModel)
+    published = models.DateField('Fecha publicación', blank=True, null=True)
+    editor = models.ForeignKey(EditorModel, on_delete=models.CASCADE)
+    description = models.TextField(max_length=300)
 
     def __str__(self):
         return self.title
--- graphene_schema/types.py
+++ graphene_schema/types.py
@@ -1,13 +1,41 @@
 import graphene
 from graphene_django.types import DjangoObjectType
-from books.models import BooksModel
+from books.models import *
+
+class AuthorsType(DjangoObjectType):
+    class Meta:
+        model = AuthorsModel
+        fields = '__all__'
+
+class CategoriesType(DjangoObjectType):
+    class Meta:
+        model = CategoriesModel
+        fields = '__all__'
+
+class EditorType(DjangoObjectType):
+    class Meta:
+        model = EditorModel
+        fields = '__all__'
 
 class BooksType(DjangoObjectType):
     class Meta:
         model = BooksModel
+        fields = '__all__'
 
 class Query(object):
+    all_authors = graphene.List(AuthorsType)
+    all_categories = graphene.List(CategoriesType)
+    all_editors = graphene.List(EditorType)
     all_books = graphene.List(BooksType)
     
+    def resolve_all_authors(self, info, **kwargs):
+        return AuthorsModel.objects.all()
+    
+    def resolve_all_categories(self, info, **kwargs):
+        return CategoriesModel.objects.all()
+    
+    def resolve_all_editors(self, info, **kwargs):
+        return EditorModel.objects.all()
+
     def resolve_all_books(self, info, **kwargs):
         return BooksModel.objects.all()
